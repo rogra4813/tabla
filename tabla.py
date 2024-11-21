ee0ac6f0fb49bf5228756f25f77db3de488439c4
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-20 21:38:22 -0500

    Update tabla.py

--- tabla.py
+++ tabla.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
 
+st.title('EJERCICIO PRÁCTICO ETL')
+st.text("*************************************************************************************************************")
 
-st.text("PRUEBA DE CSV")
+# ---------------------------------#
+# Información 
+expander_bar = st.expander("**Más sobre este trabajo**")
+expander_bar.markdown("""
+*:orange[Información:]* Este ejercicio práctico permite consolidar lo aprendido en Diseños de procesos ETL en Data Science, para ello, se realizó un web scrapping a la URL https://es.investing.com/crypto/bitcoin, para extraer la información de las cryptos.
+
+*:orange[Integrantes:]* :blue[GRUPO 6]  
+""")
+
+
 csv_url = "https://raw.githubusercontent.com/rogra4813/tabla/main/tabla_final.csv"  # Reemplaza con la URL de tu archivo CSV
 
 # Leer el archivo CSV desde la URL
