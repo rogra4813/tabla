4f0c44ae268f65e02a9d2e1e8416652388458e13
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-20 21:34:04 -0500

    Update tabla.py

--- tabla.py
+++ tabla.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 st.text("PRUEBA DE CSV")
-csv_url = "https://github.com/rogra4813/tabla/blob/main/tabla_final.csv"  # Reemplaza con la URL de tu archivo CSV
+csv_url = "https://raw.githubusercontent.com/rogra4813/tabla/main/tabla_final.csv"  # Reemplaza con la URL de tu archivo CSV
 
 # Leer el archivo CSV desde la URL
 df = pd.read_csv(csv_url)
