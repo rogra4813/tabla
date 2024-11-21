5621de8a78d7c0eda033f79ffc81e3c5be899fa2
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-20 21:41:28 -0500

    Update tabla.py

--- tabla.py
+++ tabla.py
@@ -10,7 +10,7 @@ expander_bar = st.expander("**Más sobre este trabajo**")
 expander_bar.markdown("""
 *:orange[Información:]* Este ejercicio práctico permite consolidar lo aprendido en Diseños de procesos ETL en Data Science, para ello, se realizó un web scrapping a la URL https://es.investing.com/crypto/bitcoin, para extraer la información de las cryptos.
 
-*:orange[Integrantes:]* :blue[GRUPO 6] robert   
+*:orange[Integrantes:]* :blue[GRUPO 6] Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar.   
 
 """)
 
